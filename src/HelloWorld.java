ff4ee6f1d6d4d0a67a923b11ed6bd881a9114ea4
foxmo <foxmo44@yahoo.com>
2016-07-16 12:45:07 -0500

    just added a comment

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -11,6 +11,7 @@ public class HelloWorld
 
     public static void main(String[] args)
     {
+        //The main class will create the external object and run the test
         Test objTest;
 
         objTest = new Test();
